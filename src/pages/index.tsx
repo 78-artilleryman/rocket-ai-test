58dc86b9940c0d0520e36123fec2b514a91fcad7
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-05-21 19:58:43 +0900

    design: 세번째 섹션 스타일링

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="max-w-md mx-auto bg-yellow-gradient relative overflow-hidden">
+      {/* 첫번째 섹션*/}
       <section className="relative z-50 h-[680px] overflow-hidden">
         {/* 배경 */}
         <Background />
@@ -11,6 +12,8 @@ export default function Home() {
         {/* 타이틀 */}
         <Title />
       </section>
+
+      {/* 두번째 섹션*/}
       <section className="bg-[#F3F2EF] h-110 relative">
         <div className="absolute -top-11 w-full h-50 z-50">
           <Image src="/topBalloon.svg" alt="말풍선" width={255} height={139} />
@@ -30,6 +33,34 @@ export default function Home() {
           </div>
           <Image src="/hand.png" alt="손 그림" width={120} height={285} />
         </div>
+      </section>
+
+      {/* 세번째 섹션*/}
+      <section className="w-full relative bg-[#F3F2EF] pb-20">
+        <div className="absolute -top-31 w-full h-50 z-50">
+          <Image
+            src="/bottomBalloon.svg"
+            alt="말풍선"
+            width={280}
+            height={139}
+          />
+        </div>
+        <p className="text-[#424242] text-center text-base font-normal max-w-[172px] absolute -top-18 left-14 z-50">
+          제가 oo님의 사주를
+          <br />
+          보기 쉽게 표로 정리했어요.
+        </p>
+        <div className="w-full relative">
+          <Image
+            src="/section3Character.png"
+            alt="캐릭터"
+            width={448}
+            height={306}
+          />
+          <div className="w-full h-[118px] bg-white-gradient absolute left-0 bottom-0 z-50"></div>
+        </div>
+        {/* 사주 표*/}
+        <div className="w-[350px] h-[620px] bg-red-200 -mt-5 mx-auto"></div>
       </section>
     </main>
   );
